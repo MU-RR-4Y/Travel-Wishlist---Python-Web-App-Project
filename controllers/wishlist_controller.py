1e10d4a7b5e471e7734da4af8607cda681ab3aa6
Michael Murray <mmurray24@hotmail.com>
2023-02-21 11:29:00 +0000

    added functionality to add items to wishlist from user page.

--- controllers/wishlist_controller.py
+++ controllers/wishlist_controller.py
@@ -8,22 +8,22 @@ import repositories.wishlist_repo as wishlist_repo
 wishlists_blueprint =Blueprint('wishlists', __name__)
 
 # NEW ('/new') GET
-@wishlists_blueprint.route('/wishlists/new')
-def add_wishlist():
-    destinations = destination_repo.select_all()
-    users = user_repo.select_all()
-    return render_template('/wishlists/new.html', destinations = destinations, users = users)
-
-# add a visit from the destination page
+# add a wishlist item from the destination page
 @wishlists_blueprint.route('/wishlists/new/<id>')
 def add_wishlist_from_destination(id):
     destination = destination_repo.select(id)
     users = user_repo.select_all()
     return render_template('/wishlists/new.html', destination = destination, users = users)
 
+# add a wishlist item from the user page
+@wishlists_blueprint.route('/wishlists/new/<id>/user')
+def add_wishlist_from_user(id):
+    destinations = destination_repo.select_all()
+    user = user_repo.select(id)
+    return render_template('/wishlists/new2.html', destinations = destinations, user = user)
 
 # # CREATE ('/') POST
-# # add visit from visit page
+# add wishlist
 
 @wishlists_blueprint.route('/wishlists/create', methods =['POST'])
 def create_wishlist():
@@ -33,14 +33,3 @@ def create_wishlist():
     wishlist_repo.save(wishlist)
     return redirect('/users')
     
-# # add visit from destination page
-# @wishlists_blueprint.route('/wishlists/<id>/create', methods =['POST'])
-# def create_wishlist_from_destination():
-#     user = user_repo.select(request.form['user']) 
-#     destination = destination_repo.select(request.form['destination']) 
-#     date = request.form['date']
-#     rating = request.form['rating']
-#     comment = request.form['comment']
-#     visit = Visit(user,destination,date, rating, comment)
-#     visit_repo.save(visit)
-#     return redirect('/destinations')
